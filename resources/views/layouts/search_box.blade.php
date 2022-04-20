3e716bcb7259d212825ba133472e0fd3d162c39a
Mai Viết Dũng_BT <dungvietmai@Mais-MacBook-Pro.local>
2022-04-20 09:38:33 +0700

    test deploy

--- resources/views/layouts/search_box.blade.php
+++ resources/views/layouts/search_box.blade.php
@@ -1,4 +1,4 @@
 <div class="search_box">
-    <input id="search" type="text" placeholder="Szukaj...">
+    <input id="search" type="text" placeholder="Szukaj...123">
     <button type="submit" onclick="searching($('#search').val())"><i class="fa fa-search"></i></button>
 </div>
