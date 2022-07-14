769267a354aaae3fbd24291b5c7b3d2b226f74cb
kobato-chan1912 <no_body156@yahoo.com.vn>
2022-07-14 15:14:44 +0700

    fix first ads

--- resources/views/layouts/search_box.blade.php
+++ resources/views/layouts/search_box.blade.php
@@ -2,3 +2,11 @@
     <input id="search" type="text" placeholder="Szukaj...">
     <button type="submit" onclick="searching($('#search').val())"><i class="fa fa-search"></i></button>
 </div>
+
+<div class='ads'>
+    <script>
+        let ads = $(".google-auto-placed")[0];
+        $('.ads').append(ads)
+        ads.remove()
+    </script>
+</div>
