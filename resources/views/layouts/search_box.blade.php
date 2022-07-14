dbda03002b351f281d48dad4af13d41b2877d9d5
kobato-chan1912 <no_body156@yahoo.com.vn>
2022-07-14 16:28:26 +0700

    fix ads

--- resources/views/layouts/search_box.blade.php
+++ resources/views/layouts/search_box.blade.php
@@ -4,5 +4,14 @@
 </div>
 
 <div class='google' style='padding-top: 150px;''>
-    window.addEventListener("load", loadGoogleAds);
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5750925066811073"
+    crossorigin="anonymous"></script>
+        <!-- Homepage Leaderboard -->
+    <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px"
+        data-ad-client="ca-pub-5750925066811073" data-ad-slot="5750925066">
+    </ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
 </div>
