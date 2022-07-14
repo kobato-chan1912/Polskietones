2ce45f67d98eb7f264f767f97b87b022d6e2a675
kobato-chan1912 <no_body156@yahoo.com.vn>
2022-07-15 02:38:40 +0700

    remove auto ads

--- resources/views/layouts/search_box.blade.php
+++ resources/views/layouts/search_box.blade.php
@@ -3,15 +3,3 @@
     <button type="submit" onclick="searching($('#search').val())"><i class="fa fa-search"></i></button>
 </div>
 
-<div class='google' style='padding-top: 150px;''>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5750925066811073"
-    crossorigin="anonymous"></script>
-        <!-- Homepage Leaderboard -->
-    <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px"
-        data-ad-client="ca-pub-5750925066811073" data-ad-slot="5750925066">
-    </ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-
-</div>
