83a9657292995c6a231e8e5b1c6d66fe697ae586
kobato-chan1912 <no_body156@yahoo.com.vn>
2022-08-19 17:40:46 +0700

    echo country

--- app/Http/Controllers/WebPageController.php
+++ app/Http/Controllers/WebPageController.php
@@ -35,8 +35,8 @@ class WebPageController extends Controller
         $newestSongs = Song::orderBy("id", "desc")->where("display",1)->paginate(10);
         $categories = Category::where("display",1)->get();
         $hotSongs = Song::orderBy("downloads", "desc")->where("display",1)->limit(5)->get();
-        return view ("webpage.home.home",
-            compact('post', 'newestSongs', 'categories', 'hotSongs', 'page', 'url'));
+//        return view ("webpage.home.home",
+//            compact('post', 'newestSongs', 'categories', 'hotSongs', 'page', 'url'));
     }
 
     public function download(Request $request, $id)
